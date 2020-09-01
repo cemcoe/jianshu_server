3f1f065ce2bfe1445973d83e06f557b4451881e7
cemcoe <chemxcode@gmail.com>
2020-09-01 14:57:01 +0800

    feat: 用户登录接口

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+const jsonwebtoken = require('jsonwebtoken')
+// jwt 密钥
+const { secret } = require('../config')
 const User = require('../models/users')
 
 
@@ -20,6 +23,26 @@ class UserCtl {
 
     ctx.body = user
   }
+
+  // 用户登录
+  // 需要获取用户提供的登录信息进行验证
+  // 使用jwt
+  async login(ctx) {
+    ctx.verifyParams({
+      name: { type: 'string', required: true },
+      password: { type: 'string', required: true },
+    })
+    const user = await User.findOne(ctx.request.body)
+    if (!user) {
+      ctx.throw(401, '用户名或密码不正确')
+    }
+    const { _id, name } = user
+    // 拿_id和name加密
+    const token = jsonwebtoken.sign({ _id, name }, secret, { expiresIn: '1d' })
+    // 登录成功返回token
+    ctx.body = { token }
+  }
+  
 }
 
 module.exports = new UserCtl()
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router')
 
 const router = new Router()
-const { create } = require('../controllers/users.js')
+const { create,login } = require('../controllers/users.js')
 
 
 router.get('/users', ctx => ctx.body = '获取用户列表')
@@ -9,7 +9,7 @@ router.post('/users', create)
 router.get('/users/:id', ctx => ctx.body = '获取指定用户信息')
 router.patch('/users/:id', ctx => ctx.body = '更改某个用户信息')
 router.delete('/users/:id', ctx => ctx.body = '删除用户')
-router.post('/login', ctx => ctx.body = '用户登录接口')
+router.post('/login', login)
 router.get('/users/:id/following', ctx => ctx.body = '获取用户列表')
 router.put('/users/following/:id', ctx => ctx.body = '关注某人')
 router.delete('/users/following/:id', ctx => ctx.body = '取消关注')
