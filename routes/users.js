0247918c0ad8a0819a105edd7a7eedcdd67b8ee9
cemcoe <chemxcode@gmail.com>
2020-09-01 15:42:14 +0800

    feat: 用户登录接口

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 const Router = require('koa-router')
+const jwt = require('koa-jwt')
 
 const router = new Router()
-const { create,login } = require('../controllers/users.js')
+const { create, login, checkOwner,update } = require('../controllers/users.js')
+const { secret } = require('../config')
 
+const auth = jwt({ secret })
+// jwt 生成的用户信息存放在ctx.state上
 
 router.get('/users', ctx => ctx.body = '获取用户列表')
 router.post('/users', create)
 router.get('/users/:id', ctx => ctx.body = '获取指定用户信息')
-router.patch('/users/:id', ctx => ctx.body = '更改某个用户信息')
+// 用户只有在登录状态下才能修改自己的信息
+// 两个条件 登录 自己
+router.patch('/users/:id', auth, checkOwner, update)
 router.delete('/users/:id', ctx => ctx.body = '删除用户')
 router.post('/login', login)
 router.get('/users/:id/following', ctx => ctx.body = '获取用户列表')
