a8b48d59b32f24d38b60a35d07ae7ee87de161f5
cemcoe <chemxcode@gmail.com>
2020-09-02 20:11:38 +0800

    fix: http options

--- index.js
+++ index.js
@@ -11,13 +11,25 @@ const { connectionStr } = require('./config')
 
 // 跨域问题
 app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', '*');
-  ctx.set('Access-Control-Allow-Methods', '*');
-  if (ctx.method == 'OPTIONS') {
-    ctx.body = 200;
-  } else {
+  // log request URL:
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  ctx.set("Access-Control-Max-Age", "3600");
+  ctx.set("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type,Accept");
+  ctx.set("Access-Control-Allow-Credentials", "true");
+  if (ctx.request.method == "OPTIONS") {
+    ctx.response.status = 200
+  }
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
+  try {
     await next();
+    console.log('handler通过')
+  } catch (err) {
+    console.log('handler处理错误')
+    ctx.response.status = err.statusCode || err.status || 500;
+    ctx.response.body = {
+      message: err.message
+    };
   }
 });
 
