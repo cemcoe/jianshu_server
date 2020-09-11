2d780bee15e80863999d39a066b4409064e9eb90
cemcoe <chemxcode@gmail.com>
2020-09-11 11:54:24 +0800

    build: 更改数据库地址

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  connectionStr: 'mongodb+srv://cemcoe:N7TUuOiKWJSQLm8v@zhihu.p7lwn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  connectionStr: 'mongodb+srv://cemcoe:N7TUuOiKWJSQLm8v@zhihu.p7lwn.mongodb.net/jianshu_api?retryWrites=true&w=majority',
   secret: 'jianshu_api_cemcoe',
 
 }
