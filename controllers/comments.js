5a402a8cfbc86ba9805640fc68ad87de52066ff5
cemcoe <chemxcode@gmail.com>
2020-10-06 11:14:14 +0800

    feat: 获取文章评论接口

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,6 +11,20 @@ class CommentsCtl {
     const comment = await new Comment({ ...ctx.request.body, commentator, postId }).save()
     ctx.body = comment
   }
+
+  // 获取评论列表
+  async find(ctx) {
+    const { postId } = ctx.params
+    const comments = await Comment
+      .find({ postId })
+      .populate('commentator')
+    ctx.body = {
+      status: 200,
+      data: {
+        comments
+      }
+    }
+  }
 }
 
 module.exports = new CommentsCtl()
--- routes/comments.js
+++ routes/comments.js
@@ -2,13 +2,14 @@ const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const router = new Router({ prefix: '/posts/:postId/comments' })
 
-const { create } = require('../controllers/comments')
+const { create, find } = require('../controllers/comments')
 
 const { secret } = require('../config')
 const auth = jwt({ secret })
 
 
 router.post('/', auth, create)
+router.get('/', find)
 
 
 module.exports = router
